bd22efed8f265fa75864065a9a67ff94f68f5822
mana <mnm050125@gmail.com>
2024-03-07 21:53:01 +0900

    シーン遷移

---
+++ Assets/Scene_script.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneManager.LoadScene("Main Scene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
